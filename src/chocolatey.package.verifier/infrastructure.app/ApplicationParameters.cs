483f1859a1a068cf4ce736635c6b99fb81e32cd0
Rob Reynolds <ferventcoder@gmail.com>
2015-08-19 19:39:22 -0500

    remove application parameter params
    Those items are specified in the configuration settings, so just use
    that instead.

--- src/chocolatey.package.verifier/infrastructure.app/ApplicationParameters.cs
+++ src/chocolatey.package.verifier/infrastructure.app/ApplicationParameters.cs
@@ -69,10 +69,6 @@ namespace chocolatey.package.verifier.infrastructure.app
             get { return try_get_config(() => Config.get_configuration_settings().IsDebugMode, false); }
         }
 
-        public static string GitHubUserName { get { return Config.get_configuration_settings().GitHubUserName; } }
-
-        public static string GitHubPassword { get { return Config.get_configuration_settings().GitHubPassword; } }
-
         /// <summary>
         ///   Gets a value indicating whether OVLP should insert test data. This should be false unless locally testing.
         /// </summary>
