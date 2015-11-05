b712763d5162f199a1d1dec33f1486e9d6897726
Rob Reynolds <ferventcoder@gmail.com>
2015-11-05 01:55:46 -0600

    Gist - token or user/pass

--- src/chocolatey.package.verifier/infrastructure.app/services/GistService.cs
+++ src/chocolatey.package.verifier/infrastructure.app/services/GistService.cs
@@ -53,8 +53,15 @@ namespace chocolatey.package.verifier.infrastructure.app.services
         private GitHubClient create_git_hub_client()
         {
             // TODO: What sort of error handling do we want around this?  Can we assume that these values will be correctly set?
-            //var credentials = new Credentials(_configuration.GitHubUserName, _configuration.GitHubPassword);
-            var credentials = new Credentials(_configuration.GitHubToken);
+            Credentials credentials;
+            if (!string.IsNullOrWhiteSpace(_configuration.GitHubToken))
+            {
+                credentials = new Credentials(_configuration.GitHubToken);
+            } else
+            {
+                credentials = new Credentials(_configuration.GitHubUserName, _configuration.GitHubPassword);
+            }
+
             var gitHubClient = new GitHubClient(new ProductHeaderValue("ChocolateyPackageVerifier"))
             {
                 Credentials = credentials
