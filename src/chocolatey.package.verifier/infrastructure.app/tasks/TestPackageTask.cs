9c89c29502bb246e6e4808f95f0dabb154d1ee01
Gary Ewan Park <gep13@gep13.co.uk>
2020-09-01 11:33:05 +0100

    (maint) Remove unnecessary whitespace

--- src/chocolatey.package.verifier/infrastructure.app/tasks/TestPackageTask.cs
+++ src/chocolatey.package.verifier/infrastructure.app/tasks/TestPackageTask.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright © 2015 - Present RealDimensions Software, LLC
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License at
-// 
+//
 // 	http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -163,7 +163,7 @@ namespace chocolatey.package.verifier.infrastructure.app.tasks
                                     if (e == null || string.IsNullOrWhiteSpace(e.Data)) return;
                                     this.Log().Warn(() => " [VboxManage][Error] {0}".format_with(e.Data));
                                 },
-                                null, 
+                                null,
                                 updateProcessPath: false,
                                 allowUseWindow: false);
 
